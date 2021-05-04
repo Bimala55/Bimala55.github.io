7c152d40507636a9cbe0f2264e9a999a1394170d
Bimala Basnet <bbimala85@gmail.com>
2021-05-04 15:45:08 -0400

    Added challenge

--- js/charts.js
+++ js/charts.js
@@ -110,7 +110,7 @@ function buildCharts(sample) {
       text: otu_labels,
       mode: 'markers',
       marker: {
-        color:otu_ids,        
+        color: otu_ids,
         size: sample_values,
         colorscale: 'Earth'
       }
@@ -129,20 +129,45 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    //console.log(result.wfreq);
+
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [{
+      type: "indicator",
+      mode: "gauge+number",
+      value: result.wfreq,
+      delta: { reference: 200 },
+      gauge: {
+        axis: { range: [null, 10] },
+        boarderwidth: 2,
+        boardercolor: "black",
+        bar: { color: "black" },
+        steps: [
+          { range: [0, 2], color: "red" },
+          { range: [2, 4], color: "orange" },
+          { range: [4, 6], color: "yellow" },
+          { range: [6, 8], color: "limegreen" },
+          { range: [8, 10], color: "darkgreen" }
+        ],
+        threshold: {
+          line: { color: "black", width: 10 }
+        }
+      }
+
+    }];
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = {
+      width: 500, height: 500,
+      title: "<b>Belly Button Washing Frequency</b> <br>Scrubs per Week",
+      margin: { t: 25, r: 25, l: 25, b: 25 }
+    };
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+
+
   });
 }
 
-  // 4. Create the trace for the gauge chart.
-  var gaugeData = [
-     
-  ];
-  
-  // 5. Create the layout for the gauge chart.
-  var gaugeLayout = { 
-   
-  };
-
-  // 6. Use Plotly to plot the gauge data and layout.
-  
-});
-}
